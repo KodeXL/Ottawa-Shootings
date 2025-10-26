e3d9f3062948febeadfeab9869da02b1d6e7c14f
KodeXL <141751808+KodeXL@users.noreply.github.com>
2025-10-25 22:48:30 -0400

    Simplify viewport meta tag settings
    Removed maximum and minimum scale settings from viewport meta tag.

--- sql_python/app.py
+++ sql_python/app.py
@@ -3,7 +3,7 @@ import dash_bootstrap_components as dbc
 
 app = Dash(__name__, 
            external_stylesheets= [dbc.themes.CYBORG],
-           meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0, maximum-scale = 1.2, minimum-scale=1.0'}],
+           meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0}], #maximum-scale = 1.2, minimum-scale=1.0'
            use_pages=True)
 server = app.server
 
