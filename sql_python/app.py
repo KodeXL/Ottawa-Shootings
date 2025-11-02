c0e75a73933401289b90df77f5ebcc72c2e8f525
KodeXL <141751808+KodeXL@users.noreply.github.com>
2025-11-02 18:53:55 -0500

    Add Bootstrap icons to external stylesheets

--- sql_python/app.py
+++ sql_python/app.py
@@ -2,7 +2,7 @@ from dash import Dash, dcc, html, page_registry, page_container
 import dash_bootstrap_components as dbc
 
 app = Dash(__name__, 
-           external_stylesheets= [dbc.themes.CYBORG],
+           external_stylesheets= [dbc.themes.CYBORG, dbc.icons.BOOTSTRAP],
            meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0'}], #maximum-scale = 1.2, minimum-scale=1.0'
            use_pages=True)
 server = app.server
@@ -10,7 +10,7 @@ server = app.server
 # define navigation bar
 navbar = dbc.NavbarSimple(
     children=[
-        dbc.NavItem(dbc.NavLink("Yearly Stats", href="/", external_link=True, active="exact", class_name="main-navbar")),
+        dbc.NavItem(dbc.NavLink("Overall Stats", href="/", external_link=True, active="exact", class_name="main-navbar")),
         dbc.NavItem(dbc.NavLink("Level of Injury Stats", href="/loi_stats_page", active="exact", class_name="main-navbar")),
     ],
     brand="Shootings Ottawa",
